b723287f27b31c83ee1a9c6e7b07e9243d5121a7
Sai Pc <schand31@asu.edu>
2015-08-04 21:02:28 -0700

    Added layout and buttons Added basic stop, start, reset functions

--- src/com/saipc/tda/stopwatch/stopWatchActivity.java
+++ src/com/saipc/tda/stopwatch/stopWatchActivity.java
@@ -2,14 +2,49 @@ package com.saipc.tda.stopwatch;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class stopWatchActivity extends Activity {
     /**
      * Called when the activity is first created.
      */
+    // Private variables
+    private int seconds = 0;
+    private boolean running = false;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.stopWatchLayout);
     }
+
+    public void onClickStart(View view) {
+        this.running = true;
+    }
+
+    public void onClickStop(View view) {
+        this.running = false;
+    }
+
+    public void onClickReset(View view) {
+        this.running = false;
+        this.seconds = 0;
+    }
+
+    public void runTimer(View view) {
+        TextView timeView = (TextView) findViewById(R.id.timeView);
+
+        int hours = seconds / 3600;
+        int minutes = (seconds % 3600)/ 60;
+        int secs = seconds % 60;
+        String time = String.format("%d:%2d:%2d", hours, minutes, secs);
+        timeView.setText(time);
+
+        if(running) {
+            seconds++;
+        }
+
+    }
+
+
 }
